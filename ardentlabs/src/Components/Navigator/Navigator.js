b913cfdfb95d0f40e81636cc8a8869402886d1ef
Yizhen Liu <imliuyzh@outlook.com>
2020-07-24 10:43:06 -0700

    Enhanced navdot detail

--- ardentlabs/src/Components/Navigator/Navigator.js
+++ ardentlabs/src/Components/Navigator/Navigator.js
@@ -8,64 +8,12 @@ export default function Navigator()
 {
     return (
         <div id="navigator">
-            <Link activeClass="active" to="top-section" spy={true} smooth={true}>
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
-            
-            <Link activeClass="active" to="mission-section" spy={true} smooth={true} id="mission-navdot">
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
-            
-            <Link activeClass="active" to="about-section" spy={true} smooth={true} id="about-navdot">
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
-            
-            <Link activeClass="active" to="portfolio-section" spy={true} smooth={true}>
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
-            
-            <Link activeClass="active" to="internship-section" spy={true} smooth={true} id="internship-navdot">
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
-            
-            <Link activeClass="active" to="footer" spy={true} smooth={true} id="footer-navdot" onSetActive={() => enterFooter("footer-navdot")} onSetInactive={() => leaveFooter("footer-navdot", "internship-navdot")}>
-                <svg width="12" height="12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="12" r="10" />
-                </svg>
-            </Link>
+            <Link activeClass="active" to="top-section" spy={true} smooth={true} />
+            <Link activeClass="active" to="mission-section" spy={true} smooth={true} />
+            <Link activeClass="active" to="about-section" spy={true} smooth={true} />
+            <Link activeClass="active" to="portfolio-section" spy={true} smooth={true} />
+            <Link activeClass="active" to="internship-section" spy={true} smooth={true} />
+            <Link activeClass="active" to="footer" spy={true} smooth={true} />
         </div>
     );
-}
-
-function enterFooter(destination)
-{
-    let targets = document.querySelectorAll("#navigator a");
-    targets.forEach((target) => {
-        target.className = (target.id === destination) ? "active" : "";
-    });
-}
-
-function leaveFooter(start, end)
-{
-    let targets = document.querySelectorAll("#navigator a");
-    targets.forEach((target) => {
-        if (target.id === start)
-        {
-            target.className = "";
-        }
-        else if (target.id === end)
-        {
-            target.className = "active";
-        }
-    });
 }
